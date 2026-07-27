4b35d42aca5b781591aa51ac06ca7b8f6fbd167e
An average Beanmann <102012974+AnaverageBeanmann@users.noreply.github.com>
2026-07-27 18:52:13 +0000

    Add files via upload

--- lua/entities/npc_bsz_ghoul/shared.lua
+++ lua/entities/npc_bsz_ghoul/shared.lua
@@ -3,15 +3,6 @@ ENT.Type = "ai"
 ENT.PrintName = "Ghoul"
 ENT.Author = "An average Beanmann"
 ENT.Contact = ""
-ENT.Information = "Faction: Zombies\nRank: Fodder\n\nHas a weak short-ranged throw attack.\nTakes damage when using the throw attack.\nMoves faster when close enough to an enemy.\nHas reduced melee damage.\n"
--- ENT.Information = "
--- Faction: Zombies\n
--- Rank: Fodder\n
--- \n
--- Has a weak short-ranged throw attack.\n
--- Takes damage when using the throw attack.\n
--- Moves faster when close enough to an enemy.\n
--- Has reduced melee damage.\n
--- "
-ENT.Category = "Bean's Bag of Hot Garbage"
+ENT.Information = "Standard Ghoul\nHas a weak short-ranged throw attack.\nTakes damage when using the throw attack.\nMoves faster at close range.\n"
+ENT.Category = "Bean's Sourcey Zombies"
 ENT.VJ_ID_Undead = true
